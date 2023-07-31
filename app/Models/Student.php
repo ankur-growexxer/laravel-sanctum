d25d44ac83a111b626191f2d9375f317fe028624
ankur-parkstreet <ankurpatel@parkstreet.com>
2023-07-31 19:26:47 +0530

    PO set-up

---
+++ app/Models/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'science_mark', 'english_mark', 'maths_mark'];
+}
